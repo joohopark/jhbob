86d6b4d411ee5e9bd562a8bf444ead48ed836578
joohopark <bobplease88@gmail.com>
2018-06-28 18:20:53 +0900

    push sh 수정

--- push.sh
+++ push.sh
@@ -1,6 +1,8 @@
+echo -n "push branch 입력 :"
+read branch
 echo -n "git commit 메시지 입력:"
 read input
 git add .
 git status
 git commit -m "$input"
-git push -u origin master
+git push -u origin $branch 
